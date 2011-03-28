dd400fcc6d32616aead2872950dd8419225b649c
Michal Piotrowski <michal@eventhorizon.pl>
2011-03-28 23:09:21 +0200

    update TestRunner

--- TestRunner.php
+++ TestRunner.php
@@ -7,5 +7,6 @@ passthru('php raw_pdo/TestRunner.php');
 passthru('php propel_14/TestRunner.php');
 passthru('php propel_15/TestRunner.php');
 passthru('php propel_15_with_cache/TestRunner.php');
+passthru('php propel_16/TestRunner.php');
 passthru('php doctrine_12/TestRunner.php');
 passthru('php doctrine_2/TestRunner.php');
