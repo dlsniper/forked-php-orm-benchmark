0e8e12f3553745307c746e209ce1435f10bed7fe
Michal Piotrowski <michal@eventhorizon.pl>
2012-01-30 22:02:00 +0100

    doctrine 2.2 test

--- TestRunner.php
+++ TestRunner.php
@@ -11,3 +11,4 @@ passthru('php propel_16/TestRunner.php');
 passthru('php doctrine_12/TestRunner.php');
 passthru('php doctrine_2/TestRunner.php');
 passthru('php doctrine_21/TestRunner.php');
+passthru('php doctrine_22/TestRunner.php');
---
+++ doctrine_22/vendor/doctrine2/lib/vendor/doctrine-common/tests/Doctrine/Tests/Common/Cache/MemcachedCacheTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Doctrine\Tests\Common\Cache;
+
+use Doctrine\Common\Cache\MemcachedCache;
+
+class MemcachedCacheTest extends CacheTest
+{
+    private $memcached;
+
+    public function setUp()
+    {
+        if (extension_loaded('memcached')) {
+            $this->memcached = new \Memcached();
+            $this->memcached->setOption(\Memcached::OPT_COMPRESSION, false);
+            $this->memcached->addServer('127.0.0.1', 11211);
+
+            $fh = @fsockopen('127.0.0.1', 11211);
+            if (!$fh) {
+                $this->markTestSkipped('The ' . __CLASS__ .' requires the use of memcache');
+            }
+        } else {
+            $this->markTestSkipped('The ' . __CLASS__ .' requires the use of memcache');
+        }
+    }
+
+    protected function _getCacheDriver()
+    {
+        $driver = new MemcachedCache();
+        $driver->setMemcached($this->memcached);
+        return $driver;
+    }
+}
